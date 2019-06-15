d663a0ff9f4cf273150766c82ffe9d9cef2f342a
Sean Lennaerts <seanlenny@gmail.com>
2019-06-14 17:29:12 -0700

    Updates

---
+++ precache-manifest.24ed230310aa84fa97d989c5388c6c37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a933446dd1c02305687d5b6d070d3aae",
+    "url": "/mcuverse/index.html"
+  },
+  {
+    "revision": "8e23c137898f4968ab3e",
+    "url": "/mcuverse/static/css/main.22ad6356.chunk.css"
+  },
+  {
+    "revision": "54dac966ff9eebdbcdfe",
+    "url": "/mcuverse/static/js/2.ac161d45.chunk.js"
+  },
+  {
+    "revision": "8e23c137898f4968ab3e",
+    "url": "/mcuverse/static/js/main.7807e318.chunk.js"
+  },
+  {
+    "revision": "50400f15a05ec3907403",
+    "url": "/mcuverse/static/js/runtime~main.86728467.js"
+  }
+]);
