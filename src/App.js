89c1bb14532b61c968154db08cb5a3ecdd2f5552
Sean Lennaerts <seanlenny@gmail.com>
2019-06-14 17:19:33 -0700

    pushing version 0.1 to github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import movies from './movies/index';
 
-function App() {
+
+function copyToClipboard(str) {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  const selected =
+    document.getSelection().rangeCount > 0
+      ? document.getSelection().getRangeAt(0)
+      : false;
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  if (selected) {
+    document.getSelection().removeAllRanges();
+    document.getSelection().addRange(selected);
+  }
+};
+
+function Sub(props) {
+
+  const { sub, search, title, time, handle } = props;
+  let jSub = sub.join(' ');
+
+  const handleClick = () => {
+    copyToClipboard(`${jSub} (*${title} ${time}*)`);
+    handle(true);
+    setTimeout(() => {
+      handle(false);
+    }, 2000);
+  }
+  let index = jSub.toLowerCase().indexOf(search);
+  // if (index >= 0) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="sub"
+      onClick={handleClick}
+      title="click to copy to clipboard">
+      <p>
+        {jSub.substring(0, index)}
+        <span className="highlight">{jSub.substring(index, index + search.length)}</span>
+        {jSub.substring(index + search.length)}
+        &nbsp;(<i>{title} {time}</i>)
+      </p>
     </div>
   );
+  // }
+}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { search: '', alert: false };
+
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.handleAlert = this.handleAlert.bind(this);
+  }
+
+  handleAlert(show) {
+    this.setState({alert: show});
+  }
+
+  handleSearchChange(event) {
+    this.setState({ search: event.target.value.toLowerCase().trim() });
+  }
+
+  showSubs() {
+    var matches = []
+    movies.forEach(movie => {
+      movie.subs.forEach(sub => {
+        if (sub.sub.join(' ').toLowerCase().includes(this.state.search)) {
+          matches.push(<Sub
+            sub={sub.sub}
+            search={this.state.search}
+            title={movie.title}
+            time={sub.time}
+            handle={this.handleAlert}
+          />)
+        }
+      });
+    });
+    return matches;
+  }
+
+  showCredits() {
+    return (
+      <div className="credits">
+        made by /u/shonnyboymushi
+      </div>
+    );
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="searchContainer">
+          <input
+            type="search"
+            value={this.state.value}
+            onChange={this.handleSearchChange}
+            placeholder="Search verse"
+          ></input>
+        </div>
+        <div className={`alert ${this.state.alert ? 'alert-show' : 'alert-hide'}`}>Copied to clipobard</div>
+        <div className="body">
+          {this.state.search.length > 3 ? this.showSubs() : this.showCredits()}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/movies/index.js
@@ -0,0 +1,115 @@
+import * as BlackPanther from './Black Panther.json';
+import * as CaptainMarvel from './Captain Marvel.json';
+import * as DoctorStrange from './Doctor Strange.json';
+import * as IAntMan from './I Ant-Man.json';
+import * as IAvengers from './I Avengers.json';
+import * as ICaptainAmerica from './I Captain America.json';
+import * as IGuardiansoftheGalaxy from './I Guardians of the Galaxy.json';
+import * as IIronMan from './I Iron Man.json';
+import * as ISpiderMan from './I Spider-Man.json';
+import * as IThor from './I Thor.json';
+import * as IIAntMan from './II Ant-Man.json';
+import * as IIAvengers from './II Avengers.json';
+import * as IICaptainAmerica from './II Captain America.json';
+import * as IIGuardiansoftheGalaxy from './II Guardians of the Galaxy.json';
+import * as IIIronMan from './II Iron Man.json';
+import * as IIThor from './II Thor.json';
+import * as IIIAvengers from './III Avengers.json';
+import * as IIICaptainAmerica from './III Captain America.json';
+import * as IIIIronMan from './III Iron Man.json';
+import * as IIIThor from './III Thor.json';
+import * as IVAvengers from './IV Avengers.json';
+import * as TheIncredibleHulk from './The Incredible Hulk.json';
+
+const subs = [
+    {
+        title: "Black Panther",
+        subs: BlackPanther.default
+    },
+    {
+        title: "Captain Marvel",
+        subs: CaptainMarvel.default
+    }, 
+    {
+        title: "Doctor Strange",
+        subs: DoctorStrange.default
+    },
+    {
+        title: "I Ant-Man",
+        subs: IAntMan.default
+    },
+    {
+        title: "I Avengers",
+        subs: IAvengers.default
+    },
+    {
+        title: "I Captain America",
+        subs: ICaptainAmerica.default
+    },
+    {
+        title: "I Guardians of the Galaxy",
+        subs: IGuardiansoftheGalaxy.default
+    },
+    {
+        title: "I Iron Man",
+        subs: IIronMan.default
+    },
+    {
+        title: "I Spider-Man",
+        subs: ISpiderMan.default
+    },
+    {
+        title: "I Thor",
+        subs: IThor.default
+    },
+    {
+        title: "II Ant-Man",
+        subs: IIAntMan.default
+    },
+    {
+        title: "II Avengers",
+        subs: IIAvengers.default
+    },
+    {
+        title: "II Captain America",
+        subs: IICaptainAmerica.default
+    },
+    {
+        title: "II Guardians of the Galaxy",
+        subs: IIGuardiansoftheGalaxy.default
+    },
+    {
+        title: "II Iron Man",
+        subs: IIIronMan.default
+    },
+    {
+        title: "II Thor",
+        subs: IIThor.default
+    },
+    {
+        title: "III Avengers",
+        subs: IIIAvengers.default
+    },
+    {
+        title: "III Captain America",
+        subs: IIICaptainAmerica.default
+    },
+    {
+        title: "III Iron Man",
+        subs: IIIIronMan.default
+    },
+    {
+        title: "III Thor",
+        subs: IIIThor.default
+    },
+    {
+        title: "IV Avengers",
+        subs: IVAvengers.default
+    },
+    {
+        title: "The Incredible Hulk",
+        subs: TheIncredibleHulk.default
+    }
+];
+
+export default subs;
