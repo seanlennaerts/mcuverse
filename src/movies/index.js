dbb02c07cb0fda126388767cf1dab62c1a86998a
Timendum <timedum@gmail.com>
2019-06-19 12:15:31 +0200

    Localized subs

--- src/movies/index.js
+++ src/movies/index.js
@@ -1,24 +1,24 @@
 import * as BlackPanther from './Black Panther.json';
 import * as CaptainMarvel from './Captain Marvel.json';
 import * as DoctorStrange from './Doctor Strange.json';
-import * as IAntMan from './I Ant-Man.json';
-import * as IAvengers from './I Avengers.json';
-import * as ICaptainAmerica from './I Captain America.json';
-import * as IGuardiansoftheGalaxy from './I Guardians of the Galaxy.json';
-import * as IIronMan from './I Iron Man.json';
-import * as ISpiderMan from './I Spider-Man.json';
-import * as IThor from './I Thor.json';
-import * as IIAntMan from './II Ant-Man.json';
-import * as IIAvengers from './II Avengers.json';
-import * as IICaptainAmerica from './II Captain America.json';
-import * as IIGuardiansoftheGalaxy from './II Guardians of the Galaxy.json';
-import * as IIIronMan from './II Iron Man.json';
-import * as IIThor from './II Thor.json';
-import * as IIIAvengers from './III Avengers.json';
-import * as IIICaptainAmerica from './III Captain America.json';
-import * as IIIIronMan from './III Iron Man.json';
-import * as IIIThor from './III Thor.json';
-import * as IVAvengers from './IV Avengers.json';
+import * as IAntMan from './1 Ant-Man.json';
+import * as IAvengers from './1 Avengers.json';
+import * as ICaptainAmerica from './1 Captain America.json';
+import * as IGuardiansoftheGalaxy from './1 Guardians of the Galaxy.json';
+import * as IIronMan from './1 Iron Man.json';
+import * as ISpiderMan from './1 Spider-Man.json';
+import * as IThor from './1 Thor.json';
+import * as IIAntMan from './2 Ant-Man.json';
+import * as IIAvengers from './2 Avengers.json';
+import * as IICaptainAmerica from './2 Captain America.json';
+import * as IIGuardiansoftheGalaxy from './2 Guardians of the Galaxy.json';
+import * as IIIronMan from './2 Iron Man.json';
+import * as IIThor from './2 Thor.json';
+import * as IIIAvengers from './3 Avengers.json';
+import * as IIICaptainAmerica from './3 Captain America.json';
+import * as IIIIronMan from './3 Iron Man.json';
+import * as IIIThor from './3 Thor.json';
+import * as IVAvengers from './4 Avengers.json';
 import * as TheIncredibleHulk from './The Incredible Hulk.json';
 
 const subs = [
@@ -39,92 +39,92 @@ const subs = [
     },
     {
         id: "antman1",
-        title: "I Ant-Man",
+        title: "1 Ant-Man",
         subs: IAntMan.default
     },
     {
         id: "avengers1",
-        title: "I Avengers",
+        title: "1 Avengers",
         subs: IAvengers.default
     },
     {   
         id: "captainamerica1",
-        title: "I Captain America",
+        title: "1 Captain America",
         subs: ICaptainAmerica.default
     },
     {
         id: "guardians1",
-        title: "I Guardians of the Galaxy",
+        title: "1 Guardians of the Galaxy",
         subs: IGuardiansoftheGalaxy.default
     },
     {
         id: "ironman1",
-        title: "I Iron Man",
+        title: "1 Iron Man",
         subs: IIronMan.default
     },
     {
         id: "spiderman1",
-        title: "I Spider-Man",
+        title: "1 Spider-Man",
         subs: ISpiderMan.default
     },
     {
         id: "thor1",
-        title: "I Thor",
+        title: "1 Thor",
         subs: IThor.default
     },
     {
         id: "antman2",
-        title: "II Ant-Man",
+        title: "2 Ant-Man",
         subs: IIAntMan.default
     },
     {
         id: "avengers2",
-        title: "II Avengers",
+        title: "2 Avengers",
         subs: IIAvengers.default
     },
     {
         id: "captainamerica2",
-        title: "II Captain America",
+        title: "2 Captain America",
         subs: IICaptainAmerica.default
     },
     {   
         id: "guardians2",
-        title: "II Guardians of the Galaxy",
+        title: "2 Guardians of the Galaxy",
         subs: IIGuardiansoftheGalaxy.default
     },
     {
         id: "ironman2",
-        title: "II Iron Man",
+        title: "2 Iron Man",
         subs: IIIronMan.default
     },
     {
         id: "thor2",
-        title: "II Thor",
+        title: "2 Thor",
         subs: IIThor.default
     },
     {
         id: "avengers3",
-        title: "III Avengers",
+        title: "3 Avengers",
         subs: IIIAvengers.default
     },
     {
         id: "captainamerica3",
-        title: "III Captain America",
+        title: "3 Captain America",
         subs: IIICaptainAmerica.default
     },
     {
         id: "ironman3",
-        title: "III Iron Man",
+        title: "3 Iron Man",
         subs: IIIIronMan.default
     },
     {
         id: "thor3",
-        title: "III Thor",
+        title: "3 Thor",
         subs: IIIThor.default
     },
     {   
         id: "avengers4",
-        title: "IV Avengers",
+        title: "4 Avengers",
         subs: IVAvengers.default
     },
     {
