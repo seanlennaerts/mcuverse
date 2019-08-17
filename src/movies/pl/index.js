b52463d681ef6f1258d4dd00e7168ee0fd793f07
Jakub Koralewski <jakub.koralewski1@gmail.com>
2019-08-18 00:35:33 +0200

    Add Polish subtitles and route changes.

---
+++ src/movies/pl/index.js
@@ -0,0 +1,137 @@
+import * as BlackPanther from './Black Panther.json';
+import * as CaptainMarvel from './Captain Marvel.json';
+import * as DoctorStrange from './Doctor Strange.json';
+import * as IAntMan from './I Ant-Man.json';
+import * as IAvengers from './I Avengers.json';
+import * as ICaptainAmerica from './I Captain America.json';
+import * as IGuardiansoftheGalaxy from './I Guardians of the Galaxy.json';
+import * as IIronMan from './I Iron Man.json';
+import * as ISpiderMan from './I Spider-Man.json';
+import * as IThor from './I Thor.json';
+import * as IIAntMan from './II Ant-Man.json';
+import * as IIAvengers from './II Avengers.json';
+import * as IICaptainAmerica from './II Captain America.json';
+import * as IIGuardiansoftheGalaxy from './II Guardians of the Galaxy.json';
+import * as IIIronMan from './II Iron Man.json';
+import * as IIThor from './II Thor.json';
+import * as IIIAvengers from './III Avengers.json';
+import * as IIICaptainAmerica from './III Captain America.json';
+import * as IIIIronMan from './III Iron Man.json';
+import * as IIIThor from './III Thor.json';
+import * as IVAvengers from './IV Avengers.json';
+import * as TheIncredibleHulk from './The Incredible Hulk.json';
+
+const subs = [
+    {   
+        id: "blackpanther",
+        title: "Black Panther",
+        subs: BlackPanther.default
+    },
+    {   
+        id: "captainmarvel",
+        title: "Captain Marvel",
+        subs: CaptainMarvel.default
+    }, 
+    {   
+        id: "doctorstrange",
+        title: "Doctor Strange",
+        subs: DoctorStrange.default
+    },
+    {
+        id: "antman1",
+        title: "I Ant-Man",
+        subs: IAntMan.default
+    },
+    {
+        id: "avengers1",
+        title: "I Avengers",
+        subs: IAvengers.default
+    },
+    {   
+        id: "captainamerica1",
+        title: "I Captain America",
+        subs: ICaptainAmerica.default
+    },
+    {
+        id: "guardians1",
+        title: "I Guardians of the Galaxy",
+        subs: IGuardiansoftheGalaxy.default
+    },
+    {
+        id: "ironman1",
+        title: "I Iron Man",
+        subs: IIronMan.default
+    },
+    {
+        id: "spiderman1",
+        title: "I Spider-Man",
+        subs: ISpiderMan.default
+    },
+    {
+        id: "thor1",
+        title: "I Thor",
+        subs: IThor.default
+    },
+    {
+        id: "antman2",
+        title: "II Ant-Man",
+        subs: IIAntMan.default
+    },
+    {
+        id: "avengers2",
+        title: "II Avengers",
+        subs: IIAvengers.default
+    },
+    {
+        id: "captainamerica2",
+        title: "II Captain America",
+        subs: IICaptainAmerica.default
+    },
+    {   
+        id: "guardians2",
+        title: "II Guardians of the Galaxy",
+        subs: IIGuardiansoftheGalaxy.default
+    },
+    {
+        id: "ironman2",
+        title: "II Iron Man",
+        subs: IIIronMan.default
+    },
+    {
+        id: "thor2",
+        title: "II Thor",
+        subs: IIThor.default
+    },
+    {
+        id: "avengers3",
+        title: "III Avengers",
+        subs: IIIAvengers.default
+    },
+    {
+        id: "captainamerica3",
+        title: "III Captain America",
+        subs: IIICaptainAmerica.default
+    },
+    {
+        id: "ironman3",
+        title: "III Iron Man",
+        subs: IIIIronMan.default
+    },
+    {
+        id: "thor3",
+        title: "III Thor",
+        subs: IIIThor.default
+    },
+    {   
+        id: "avengers4",
+        title: "IV Avengers",
+        subs: IVAvengers.default
+    },
+    {
+        id: "hulk",
+        title: "The Incredible Hulk",
+        subs: TheIncredibleHulk.default
+    }
+];
+
+export default subs;
