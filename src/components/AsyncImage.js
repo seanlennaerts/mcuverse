77a4147c74879a7d36ef06090aff2d6b6a9fb93f
Sean Lennaerts <seanlenny@gmail.com>
2019-08-23 20:32:35 -0700

    async image loading smoother transition

--- src/components/AsyncImage.js
+++ src/components/AsyncImage.js
@@ -23,7 +23,7 @@ class AsyncImage extends Component {
   render() {
     return(
       <a title="Download image" href={`https://firebasestorage.googleapis.com/v0/b/searchmoviequotes.appspot.com/o/starwars%2F${this.props.src.split('/')[1]}%2F${this.props.src.split('/')[2]}?alt=media`} rel="noopener noreferrer" download target="_blank">
-        <div className="asyncWrapper" style={this.state.loaded ? {backgroundImage: `url(/assets/${this.props.spl})`} : {backgroundImage: `url(/assets/${this.props.src})`, animation: "pulse 0.7s infinite ease-in-out"}}>
+        <div className={`asyncWrapper ${this.state.loaded ? '' : 'pulse'}`} style={{backgroundImage: `url(/assets/${this.props.src})`}}>
           <img className="remote" onLoad={this.onLoad} style={this.state.loaded ? {opacity: 1} : {opacity: 0}} src={`https://firebasestorage.googleapis.com/v0/b/searchmoviequotes.appspot.com/o/starwars%2F${this.props.src.split('/')[1]}%2F${this.props.src.split('/')[2]}?alt=media`} alt={this.props.text.join(' ')}/>
         </div>
       </a>
