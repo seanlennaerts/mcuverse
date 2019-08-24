d8deaef57b19dbeec79b5331f58be63c29413ecd
Sean Lennaerts <seanlenny@gmail.com>
2019-08-23 19:32:35 -0700

    changed a to links

--- src/routes/Franchise.js
+++ src/routes/Franchise.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import '../styles/Franchise.scss';
+import { Link } from 'react-router-dom';
 
 function Franchise() {
   return (
     <div>
       <div className="rootWrapper">
-        <a href="/mcuverse" title="Marvel Cinematic Universe">
+        <Link to="/mcuverse" title="Marvel Cinematic Universe">
           <div className="franchiseWrapper" style={{ backgroundColor: '#19181f' }}>
             <img src='/assets/Marvel_Studios_2016_logo.svg' alt='MCU logo' />
           </div>
-        </a>
-        <a href="/starwars" title = "Star Wars">
+        </Link>
+        <Link to="/starwars" title = "Star Wars">
           <div className="franchiseWrapper" style={{ backgroundColor: 'black' }}>
             <p className="franchiseName starwars">Star Wars</p>
           </div>
-        </a>
+        </Link>
       </div>
       <div className="credit">
         <a href="https://github.com/seanlennaerts/" target="_blank" rel="noopener noreferrer">Sean Lennaerts</a>
