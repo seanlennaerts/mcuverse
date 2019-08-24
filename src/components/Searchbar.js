065822852ce14665397293b1dc7c6aa2213508be
Sean Lennaerts <seanlenny@gmail.com>
2019-08-24 01:24:42 -0700

    added home button

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 import '../styles/Searchbar.scss';
 function Searchbar(props) {
 
   const { value, onSearchChange, placeholder } = props;
   return (
-    <div className="searchContainer">
-      <input
-        id="search"
-        type="search"
-        value={value}
-        onChange={onSearchChange}
-        placeholder={placeholder}
-        autoFocus
-        autoComplete="off"
-      />
+    <div className="searchmenuContainer">
+      <div className="menu"><Link to="/"><FontAwesomeIcon icon={faBars} /></Link></div>
+      <div className="searchContainer">
+        <input
+          id="search"
+          type="search"
+          value={value}
+          onChange={onSearchChange}
+          placeholder={placeholder}
+          autoFocus
+          autoComplete="off"
+        />
+      </div>
     </div>
+
   );
 }
 
